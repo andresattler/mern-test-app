18cf3aca334a4d0b51099d7bd29b504c6254a541
andresattler <andresattler.webdev@gmail.com>
2017-05-17 16:11:18 +1200

    initial commit with react and express

---
+++ server.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+
+var app = express();
+
+// VIEW ENGINE
+app.set('view engine', 'html');
+app.engine('html', function (path, options, callback) {
+  fs.read(path, 'utf-8', callback);
+})
+
+// MIDDLEWARE
+app.use(express.static(__dirname));
+
+// ROUTES
+app.get('', function (req, res){
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// ERROR HANDLER
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+});
+
+// SERVE APP
+ var port = 8000;
+ app.listen(port, function(){
+   console.log('running at localhost:' + port);
+ });
